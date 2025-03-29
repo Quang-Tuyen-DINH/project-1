566a68fbe8309133812985ed3fba72cf2a0d8840
Quang Tuyen Dinh <qtuyen43@gmail.com>
2025-03-29 21:44:59 +0100

    feat: add routes todos to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import routes from "./routes";
+import todosRouter from "./routes/todo.routes";
 
 const app = express();
 const port = 3000;
 
-app.use("/", routes);
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+app.use("/api", todosRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
